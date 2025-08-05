b798a39b688fdb3bd21d59c8bbec82bed8935ad6
Kate <>
2025-08-05 11:45:31 +0300

    add session history

--- app/page.tsx
+++ app/page.tsx
@@ -3,43 +3,31 @@ import CompanionCard from "@/components/CompanionCard";
 import CTA from "@/components/CTA";
 import CompanionsList from "@/components/CompanionsList";
 import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+    const companions = await getAllCompanions({limit: 3})
+    const resentSessionsCompanions = await getRecentSessions()
+
     return (
         <main>
             <h1 className="text-2xl underline">Popular Companions</h1>
 
             <section className="home-section">
-                <CompanionCard
-                    id='123'
-                    name="Neura the Brainy Explorer"
-                    topic="Neural Network of the Brain"
-                    subject="science"
-                    duration={45}
-                    color="#E5D0FF"
-                />
-                <CompanionCard
-                    id='456'
-                    name="Countsy the Number Wizard"
-                    topic="Derivatives & Integrals"
-                    subject="maths"
-                    duration={30}
-                    color="#ffda6e"
-                />
-                <CompanionCard
-                    id='789'
-                    name="Verba the Vocabulary Builder"
-                    topic="English Literature"
-                    subject="language"
-                    duration={30}
-                    color="#BDE7FF"
-                />
+                {companions.map((companion) => (
+                    <CompanionCard
+                        key={companion.id}
+                        {...companion}
+                        color={getSubjectColor(companion.subject)}
+                    />
+                ))}
             </section>
 
             <section className="home-section">
                 <CompanionsList
                     title="Recently Completed Sessions"
-                    companions={recentSessions}
+                    companions={resentSessionsCompanions}
                     classNames="w-2/3 max-lf:w-full"
                 />
                 <CTA/>
